7b37deaf438e7bdea3df4e2cd8d4d0ff28b1d80f
Rahul Mamgain <Rahul.Mamgain@informa.com>
2018-01-31 17:57:15 +0530

    Commiting properties to eureka client

--- 1.1.147/scripts/start.sh
+++ 1.1.147/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash /scripts/build_config.sh /tomcat/webapps/eureka/WEB-INF/classes/eureka-client.properties
+bash /scripts/build_config.sh /tomcat/webapps/eureka/WEB-INF/classes/eureka-client-$1.properties
 
 bash /tomcat/bin/catalina.sh run
