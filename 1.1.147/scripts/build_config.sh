df88654b9761f828d3a05875632dcefe1949130a
Rahul Mamgain <Rahul.Mamgain@informa.com>
2018-01-31 16:32:53 +0530

    Adding a /n

--- 1.1.147/scripts/build_config.sh
+++ 1.1.147/scripts/build_config.sh
@@ -5,6 +5,7 @@ CONFIG_FILE="$1"
 for VAR in `env`
 do
     if [[ $VAR =~ ^EUREKA_ ]]; then
+        echo -e "\n" >> $CONFIG_FILE
         echo $VAR
         eureka_name=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ . | tr 0 - `
         echo $eureka_name
@@ -12,7 +13,7 @@ do
         if egrep -q "(^|^#)$eureka_name=" $CONFIG_FILE; then
             sed -r -i "s@(^|^#)($eureka_name)=(.*)@\2=${!env_var}@g" $CONFIG_FILE #note that no config values may contain an '@' char
         else
-            echo "\n$eureka_name=${!env_var}" >> $CONFIG_FILE
+            echo -e "$eureka_name=${!env_var}" >> $CONFIG_FILE
         fi
     fi
 done
