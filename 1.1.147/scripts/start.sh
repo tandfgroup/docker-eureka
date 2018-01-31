d19a3bede6411ee71f76e523821f2dc442a2815e
Rahul Mamgain <Rahul.Mamgain@informa.com>
2018-01-31 19:27:30 +0530

    Exporting Tomcat Options So That Eureka Knows It's ON the cloud

--- 1.1.147/scripts/start.sh
+++ 1.1.147/scripts/start.sh
@@ -2,6 +2,6 @@
 
 bash /scripts/build_config.sh /tomcat/webapps/eureka/WEB-INF/classes/eureka-client-dev.properties
 
-export TOMCAT_OPTS=-Deureka.datacenter=cloud
+export CATALINA_OPTS=-Deureka.datacenter=cloud
 
 bash /tomcat/bin/catalina.sh run
