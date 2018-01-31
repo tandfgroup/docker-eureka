8b233ce931ba0effff92343a459b15c702423d4e
Rahul Mamgain <Rahul.Mamgain@informa.com>
2018-01-31 18:17:27 +0530

    Writing To Test Client Properties

--- 1.1.147/scripts/start.sh
+++ 1.1.147/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash /scripts/build_config.sh /tomcat/webapps/eureka/WEB-INF/classes/eureka-client-$1.properties
+bash /scripts/build_config.sh /tomcat/webapps/eureka/WEB-INF/classes/eureka-client-test.properties
 
 bash /tomcat/bin/catalina.sh run
